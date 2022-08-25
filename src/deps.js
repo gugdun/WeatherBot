d112e8d5dd6eac0704f2ba92c4d188f4fe7d4cac
gugdun <gugdunoff@gmail.com>
2022-08-25 18:46:25 +0300

    Add database support

--- src/deps.js
+++ src/deps.js
@@ -5,8 +5,12 @@ const Users = require('./models/Users');
 const DInjector = require('./core/DInjector');
 const di = new DInjector();
 
+const { Sequelize } = require('sequelize');
+const db = new Sequelize(process.env.DATABASE);
+
+di.add('db', () => db);
 di.add('geocoding', () => new Geocoding());
 di.add('forecasts', () => new Forecasts());
-di.add('users', () => new Users());
+di.add('users', () => di.inject(Users));
 
 module.exports = di;
--- src/models/Users.js
+++ src/models/Users.js
@@ -1,1 +1,27 @@
-module.exports = class Users {};
+const { Sequelize } = require('sequelize');
+const Coordinates = require("../entities/Coordinates");
+
+module.exports = class Users {
+  constructor(/** @type {Sequelize} */ db) {
+    db.authenticate()
+      .then((value) => console.log('Connection has been established successfully'))
+      .catch((reason) => console.log('Unable to connect to the database'));
+  }
+
+  async create(userId) {
+
+  }
+
+  async remove(userId) {
+
+  }
+
+  async setLocation(userId, /** @type {Coordinates} */ location) {
+
+  }
+
+  /** @returns {Coordinates} */
+  async getLocation(userId) {
+
+  }
+};
