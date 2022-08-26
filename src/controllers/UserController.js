a21c9c229d2e2e6b4481a8b79d12fb85f987bb35
gugdun <gugdunoff@gmail.com>
2022-08-26 19:06:20 +0300

    Update user messages

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -14,20 +14,17 @@ module.exports = di.inject(class UserController {
 
   async create(/** @type {Request} */ req, /** @type {Response} */ res) {
     const created = await this.#users.saveUser(req.userId);
-    if (created) {
-      res.sendMessage(`User ${req.userId} was created!`);
-    } else {
-      res.sendMessage(`User ${req.userId} already exists.`);
-    }
+    if (created) res.sendMessage(`
+Use /now Minsk to show the current weather in Minsk.
+Use /tomorrow Minsk to show tomorrow's weather in Minsk.
+You can send your geolocation to the bot and then the commands will work without specifying the city.
+To stop working with the bot and delete all your data, use the /stop command.
+    `);
   }
 
   async remove(/** @type {Request} */ req, /** @type {Response} */ res) {
     const removed = await this.#users.removeUser(req.userId);
-    if (removed) {
-      res.sendMessage(`User ${req.userId} was removed!`);
-    } else {
-      res.sendMessage(`User ${req.userId} does not exist.`);
-    }
+    if (removed) res.sendMessage(`See you later! \u{1f44b}`);
   }
 
   async setLocation(/** @type {Request} */ req, /** @type {Response} */ res) {
@@ -39,6 +36,6 @@ module.exports = di.inject(class UserController {
         longitude: req.params[1]
       })
     );
-    res.sendMessage(`User ${req.userId} location was changed!`);
+    res.sendMessage(`Your location was changed!`);
   }
 });
